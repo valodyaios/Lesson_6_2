50286ccaec91e98fa7c240990cac670734b89ba4
valodyaios <valodyagalstyan89.ios@gmail.com>
2024-01-25 21:43:44 +0400

    Initial commit for adding new project

---
+++ Lesson_6_2/Lesson_6_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_6_2
+//
+//  Created by Andrey Lupin on 25.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
